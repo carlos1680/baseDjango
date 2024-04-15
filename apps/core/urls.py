174fb531510d1aaee4d99b4702c38e4c18654520
carlos1680 <cpiriz@d1030comp7linux.imd.net>
2024-04-15 16:16:12 -0300

    Algo es algo

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, home,products
 
 urlpatterns = [
     #path('testbasico',index,name="index"),
-    path('',index,name="index"),
+    path('index',index,name="index"),
+    path('products',products,name="products"),
+    path('home',home,name="home"),
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -6,3 +6,11 @@ from django.template import loader
 def index(request):
     template = loader.get_template('core/base.html')
     return HttpResponse(template.render())
+
+def home(request):
+    template = loader.get_template('core/home.html')
+    return HttpResponse(template.render())
+
+def products(request):
+    template = loader.get_template('core/products.html')
+    return HttpResponse(template.render())
